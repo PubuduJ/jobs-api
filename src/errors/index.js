17c1249373293aa384847b83dab49f7d4fe0ea9d
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-11 22:47:01 +0530

    Add error classes

---
+++ src/errors/index.js
@@ -0,0 +1,6 @@
+const CustomAPIError = require('./customAPIError')
+const UnauthenticatedError = require('./unAuthenticatedError')
+const NotFoundError = require('./notFoundError')
+const BadRequestError = require('./badRequestError')
+
+module.exports = {CustomAPIError, UnauthenticatedError, NotFoundError, BadRequestError,}
