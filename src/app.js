bc05c84ea09a3059049c8cc7cd460eb6122c54d9
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-12 11:47:35 +0530

    Add security packages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require("helmet");
 const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require('express');
 const app = express();
 const connectDB = require("./db/connect");
@@ -12,8 +18,17 @@ const notFoundMiddleware = require('./middlewares/notFoundMiddleware');
 const errorHandlerMiddleware = require('./middlewares/errorHandlerMiddleware');
 const authenticationMiddleware = require("./middlewares/authenticationMiddleware");
 
-app.use(cors());
+
+app.use(rateLimiter(
+    {
+        windowMs: 15 * 60 * 1000,   // 15 minutes
+        limit: 100                  // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    }
+));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
