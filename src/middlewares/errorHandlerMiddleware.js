5a26c73430da3b3f26549e2afba30647f3f991f2
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-12 11:34:18 +0530

    Revamp errorHandlerMiddleware

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -6,8 +6,24 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || "Something went wrong, try again later"
   }
+
+  // Handle mongoose cast errors
+  if (err.name === "CastError") {
+    customError.statusCode = StatusCodes.NOT_FOUND;
+    customError.message = `No item found with id: ${err.value}`;
+  }
+
+  // Handle mongoose validation errors
+  if (err.name === "ValidationError") {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = Object.values(err.errors).map((item) => {
+      return item.message;
+    }).join(", ");
+  }
+
+  // Handle mongoose duplicate errors
   if (err.code && err.code === 11000) {
-    customError.statusCode = 400;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
     customError.message = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
   }
   return res.status(customError.statusCode).json({ message:customError.message });
