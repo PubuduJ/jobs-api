e50e07fa34423961097a93d093049a8fd0a21cd9
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-12 13:27:07 +0530

    Improvements

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ require('express-async-errors');
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
-const rateLimiter = require("express-rate-limit");
 
 const express = require('express');
 const app = express();
@@ -18,13 +17,6 @@ const notFoundMiddleware = require('./middlewares/notFoundMiddleware');
 const errorHandlerMiddleware = require('./middlewares/errorHandlerMiddleware');
 const authenticationMiddleware = require("./middlewares/authenticationMiddleware");
 
-
-app.use(rateLimiter(
-    {
-        windowMs: 15 * 60 * 1000,   // 15 minutes
-        limit: 100                  // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    }
-));
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
