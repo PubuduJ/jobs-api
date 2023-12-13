bc3b52cd65797306b65bb3c471fb50fb2981ec5e
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-13 14:55:30 +0530

    Update notFoundMiddleware

--- src/middlewares/notFoundMiddleware.js
+++ src/middlewares/notFoundMiddleware.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 const notFoundMiddleware = (req, res) => {
-    res.status(StatusCodes.NOT_FOUND).send('Route does not exist');
+    res.status(StatusCodes.NOT_FOUND).json({message: 'Route does not exist'});
 }
 
 module.exports = notFoundMiddleware;
